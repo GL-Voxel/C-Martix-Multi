336d9b71f05a515da309a155a8f52447fd4e56c7
Sword Pencil <34251020+SwordPencil@users.noreply.github.com>
2018-02-14 16:33:36 +0200

    Update matrix.cs

--- matrix.cs
+++ matrix.cs
@@ -6,29 +6,33 @@ using System.Threading.Tasks;
 
 namespace VectorSpace
 {
-    public class Martix
+    public class Matrix
     {//Класс: Матрица
-        uint numOfStrings, 
+        uint numOfRows, 
             numOfColumns;
         float[][] values;
 
         public Matrix()
         {
-            numOfStrings = numOfColumns = 0;
+            numOfRows = numOfColumns = 0;
             values = null;
         }
 
-        public static Martix operator *(Matrix m1, Matrix m2)
+        public static Matrix operator *(Matrix m1, Matrix m2)
         {
-            for(uint i = 0; i < m1.numberOfStrings; i++)
-            return m1;
+            Matrix result = new Matrix(m2.numOfRows, m1.numOfColumns, null);
+            for (uint i = 0; i < result.numOfRows; i++)
+                for (uint j = 0; j < result.numOfRows; j++)
+                    for (uint k = 0; k < m1.numOfRows; k++)
+                        result.values[i][j] += m1.values[i][k] * m2.values[k][i];
+            return result;
         }
 
-        public Matrix(uint s, uint c, float[][] vals)
+        public Matrix(uint r, uint c, float[][] vals)
         {
-            numOfStrings = s;
+            numOfRows = r;
             numOfColumns = c;
-            for (uint i = 0; i < s; i++)
+            for (uint i = 0; i < r; i++)
                 for (uint j = 0; j < c; j++)
                     values[i][j] = vals[i][j]; 
         }
