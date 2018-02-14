5ce5b2d2b073ab7f74e561b4eed56be736d17ad7
Sword Pencil <34251020+SwordPencil@users.noreply.github.com>
2018-02-14 20:20:58 +0200

    Update matrix.cs

--- matrix.cs
+++ matrix.cs
@@ -4,13 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VectorSpace
-{
-    public class Matrix
-    {//Класс: Матрица
-        uint numOfRows, 
-            numOfColumns;
-        float[][] values;
+namespace VectorSpace{
+    public class Matrix{//Класс: Матрица
+        private uint numOfRows, //Количество строк в матрице
+            numOfColumns; //Количество столбцов в матрице
+        private double[ , ] values; //Значения элементов матрицы
 
         public Matrix()
         {
@@ -18,23 +16,27 @@ namespace VectorSpace
             values = null;
         }
 
-        public static Matrix operator *(Matrix m1, Matrix m2)
-        {
-            Matrix result = new Matrix(m2.numOfRows, m1.numOfColumns, null);
-            for (uint i = 0; i < result.numOfRows; i++)
-                for (uint j = 0; j < result.numOfRows; j++)
-                    for (uint k = 0; k < m1.numOfRows; k++)
-                        result.values[i][j] += m1.values[i][k] * m2.values[k][i];
-            return result;
-        }
-
-        public Matrix(uint r, uint c, float[][] vals)
-        {
+        public Matrix(uint r, uint c, double[ , ] vals){
             numOfRows = r;
             numOfColumns = c;
             for (uint i = 0; i < r; i++)
                 for (uint j = 0; j < c; j++)
-                    values[i][j] = vals[i][j]; 
+                    values[i, j] = vals[i, j];
         }
+
+        public double[,] GetValues()
+        {
+            return values;
+        }
+
+        public static Matrix operator *(Matrix m1, Matrix m2){//Перегрузка умножения для двух матриц
+            Matrix result = new Matrix(m2.numOfRows, m1.numOfColumns, null);
+            for (uint i = 0; i < result.numOfRows; i++)
+                for (uint j = 0; j < result.numOfRows; j++)
+                    for (uint k = 0; k < m1.numOfRows; k++)
+                        result.values[i, j] += m1.values[i, k] * m2.values[k, i];
+            return result;
+        }
+
     }
 }
