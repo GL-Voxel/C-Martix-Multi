e75b4d938ee700c5f5ead18c8b89d945ac8843b9
Sword Pencil <34251020+SwordPencil@users.noreply.github.com>
2018-02-14 19:19:09 +0200

    Create Programm.cs

---
+++ Programm.cs
@@ -0,0 +1,13 @@
+using System;
+using VectorSpace;
+
+class App
+{
+    static void Main(string[] args)
+    {
+        Matrix m1 = new Matrix(),
+            m2 = new Matrix(),
+            testMult = m1 * m2;
+
+    }
+}
