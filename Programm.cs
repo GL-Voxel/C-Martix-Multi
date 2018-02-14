61f15e2ab90a3512a763f1127f298564880e7d1e
Sword Pencil <34251020+SwordPencil@users.noreply.github.com>
2018-02-14 20:21:23 +0200

    Update Programm.cs

--- Programm.cs
+++ Programm.cs
@@ -5,9 +5,28 @@ class App
 {
     static void Main(string[] args)
     {
-        Matrix m1 = new Matrix(),
-            m2 = new Matrix(),
-            testMult = m1 * m2;
-
+        Matrix[] matricies = new Matrix[2];
+        for (uint i = 0; i < 2; i++)
+        {//В этом цикле происходит ввод и вывод матриц.
+            uint rows = 0,
+            columns = 0;
+            Console.WriteLine("Сколько строк будет в матрице 1? ");
+            rows = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Сколько строк будет в матрице 2? ");
+            columns = Convert.ToUInt32(Console.ReadLine());
+            double[,] values = new double[rows, columns];
+            for (uint j = 0; j < rows; j++)
+                for (uint k = 0; k < columns; k++)
+                {
+                    Console.WriteLine("Введите значение ", j++, " ", i++, " ");
+                    values[j, k] = Convert.ToDouble(Console.ReadLine());
+                }
+            matricies[i] = new Matrix(rows, columns, values);
+            foreach (double value in matricies[i].GetValues())
+                Console.WriteLine(value);
+        }
+        Matrix multiResult = matricies[0] * matricies[1]; //Тестируем умножение двух матриц.
+            foreach(double value in multiResult.GetValues())
+                Console.WriteLine(value);
     }
 }
