ac8791d215ae96ae2f5250ff0c6f43b649e6c2bf
Sword Pencil <34251020+SwordPencil@users.noreply.github.com>
2018-02-14 20:33:54 +0200

    Update Programm.cs

--- Programm.cs
+++ Programm.cs
@@ -8,17 +8,17 @@ class App
         Matrix[] matricies = new Matrix[2];
         for (uint i = 0; i < 2; i++)
         {//В этом цикле происходит ввод и вывод матриц.
-            uint rows = 0,
+            UInt32 rows = 0,
             columns = 0;
-            Console.WriteLine("Сколько строк будет в матрице 1? ");
+            Console.WriteLine("Сколько строк будет в матрице #", i ++, "? ");
             rows = Convert.ToUInt32(Console.ReadLine());
-            Console.WriteLine("Сколько строк будет в матрице 2? ");
+            Console.WriteLine("Сколько строк будет в матрице #", i ++, "? ");
             columns = Convert.ToUInt32(Console.ReadLine());
             double[,] values = new double[rows, columns];
-            for (uint j = 0; j < rows; j++)
-                for (uint k = 0; k < columns; k++)
+            for (UInt32 j = 0; j < rows; j++)
+                for (UInt32 k = 0; k < columns; k++)
                 {
-                    Console.WriteLine("Введите значение ", j++, " ", i++, " ");
+                    Console.Write("Введите значение #", j + 1, " ", i + 1, "\n");
                     values[j, k] = Convert.ToDouble(Console.ReadLine());
                 }
             matricies[i] = new Matrix(rows, columns, values);
