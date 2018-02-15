b5cd4d8bf5e58d0251864ccec90a0fb0feebbc20
Sword Pencil <34251020+SwordPencil@users.noreply.github.com>
2018-02-15 14:24:06 +0200

    Update matrix.cs

--- matrix.cs
+++ matrix.cs
@@ -1,10 +1,10 @@
 using System;
 
-public class Matrix
-{//Класс: Матрица
+public class Matrix<Type>
+{//Класс-шаблон: Матрица.
     private uint numOfRows, //Количество строк в матрице
         numOfColumns; //Количество столбцов в матрице
-    private double[,] values; //Значения элементов матрицы
+    private Type[,] values; //Значения элементов матрицы. Тип значения может быть разный
 
     public Matrix()
     {
@@ -12,28 +12,28 @@ public class Matrix
         values = null;
     }
 
-    public Matrix(uint r, uint c, double[,] vals)
+    public Matrix(uint r, uint c, Type[,] vals)
     {
         numOfRows = r;
         numOfColumns = c;
-        for (uint i = 0; i < r; i++)
-            for (uint j = 0; j < c; j++)
+        values = new Type[r, c];
+        for (uint i = 0; i < numOfRows; i++)
+            for (uint j = 0; j < numOfColumns; j++)
                 values[i, j] = vals[i, j];
     }
 
-    public double[,] GetValues()
+    public Type[,] GetValues()
     {
         return values;
     }
 
-    public static Matrix operator *(Matrix m1, Matrix m2)
+    public static Matrix<Type> operator *(Matrix<Type> m1, Matrix<Type> m2)
     {//Перегрузка умножения для двух матриц
-        Matrix result = new Matrix(m2.numOfRows, m1.numOfColumns, null);
+        Matrix<Type> result = new Matrix<Type>(m2.numOfRows, m1.numOfColumns, null);
         for (uint i = 0; i < result.numOfRows; i++)
             for (uint j = 0; j < result.numOfRows; j++)
                 for (uint k = 0; k < m1.numOfRows; k++)
                     result.values[i, j] += m1.values[i, k] * m2.values[k, i];
         return result;
     }
-
 }
